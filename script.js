aa1143d3e80021a7bde1aec10e73df32e8280022
arc486 <77684570+arc486@users.noreply.github.com>
2021-02-26 17:35:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var  userChoice = prompt (" ! tails ")
+
+let randomNumber = 1; 
+
+var y = (Math.random();
+if (y < 0.5)
+	y = 0 
+else
+	y= 1
+
+
+
+const computerChoice = tails ; 
+
+if(computerChoice) {
+    alert("you guessed right! the coin flip landed on tails");
+} 
+
+
+alert("You guessed right! he coin flip landed on {tials}")
+
+
+let birthyear = true
+
+prompt(" My birth year is " + birthyear);
+ 
+const year = 2005-2021
+]
+var age = 16
+
+if(birthyear) {
+   alert("you are old enough to drink in the US") 
+}
+
+
+
+
+
